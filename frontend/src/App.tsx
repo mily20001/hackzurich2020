bda8588ea83b1b563448b9b7464099839cbec29e
mily20001 <mily20001@gmail.com>
2020-09-19 07:41:56 +0200

    add timeline

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,13 +4,15 @@ import './App.css';
 import 'antd/dist/antd.dark.css';
 import styled from 'styled-components';
 import { Redirect, Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import { CalendarOutlined } from '@ant-design/icons';
 
 import BackgroundMap from './components/BackgroundMap';
 
 // https://github.com/PaulLeCam/react-leaflet/issues/255
 import L from 'leaflet';
 import { Canton } from './components/cantons';
-import { Slider } from 'antd';
+import { Button, DatePicker, Modal, Slider } from 'antd';
+import moment from 'moment';
 // @ts-ignore
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
@@ -20,7 +22,14 @@ L.Icon.Default.mergeOptions({
 });
 
 function App() {
+  const baseDate = moment('2020-03-13');
+  const diffDays = moment().diff(baseDate, 'days');
+
   const [activeCanton, setActiveCanton] = useState<Canton | undefined>();
+  const [sliderValue, setSliderValue] = useState(diffDays);
+  const [calendarModalVisible, setCalendarModalVisible] = useState(false);
+
+  const displayedDate = moment(baseDate).add(sliderValue, 'days');
 
   return (
     <div className="App">
@@ -40,10 +49,12 @@ function App() {
           {/*</Route>*/}
           <Route exact path="/">
             <ModuleContainer>
-              <div style={{ width: '100%', pointerEvents: 'auto' }}>
+              <div style={{ width: '100%' }}>
                 <InfoBoxContainer>
                   {activeCanton ? <InfoBox>
+                      <div>Data at {displayedDate.format('DD-MM-YYYY')}:</div>
                       <div>Canton: {activeCanton}</div>
+                      <div>Population: {Math.round(Math.random() * 100000)}</div>
                       <div>Active cases: {Math.round(Math.random() * 1000)}</div>
                       <div>Total cases: {Math.round(Math.random() * 5000)}</div>
                       <div>Fatal cases: {Math.round(Math.random() * 500)}</div>
@@ -56,8 +67,39 @@ function App() {
             </ModuleContainer>
             <ModuleContainer>
               <TimeSliderContainer>
-                <StyledSlider range defaultValue={[50, 50]} />
+                <CalendarButtonContainer>
+                  <CalendarButton>
+                    <Button icon={<CalendarOutlined />} shape='circle-outline' size='large'
+                            onClick={() => setCalendarModalVisible(true)} />
+                  </CalendarButton>
+                </CalendarButtonContainer>
+
+                <SliderWrapper>
+                  <SliderLegendContainer>
+                    <div>{baseDate.format('DD-MM-YYYY')}</div>
+                    <div>{moment().format('DD-MM-YYYY')}</div>
+                  </SliderLegendContainer>
+                  <StyledSlider
+                    value={sliderValue}
+                    max={diffDays}
+                    onChange={(value: number) => setSliderValue(value)}
+                    tipFormatter={(value) => moment(baseDate).add(value, 'days').format('DD-MM-YYYY')}
+                  />
+                </SliderWrapper>
+                <div style={{ flexGrow: 1 }} />
               </TimeSliderContainer>
+              <Modal visible={calendarModalVisible} onCancel={() => setCalendarModalVisible(false)}
+                     title='Select date of data to display'
+                     footer={[<Button type="primary" onClick={() => setCalendarModalVisible(false)}>
+                       OK
+                     </Button>]}>
+                <DatePicker value={displayedDate}
+                            inputReadOnly
+                            size='large'
+                            allowClear={false}
+                            onChange={value => value && setSliderValue(value.diff(baseDate, 'days'))}
+                            disabledDate={date => !date.isBetween(baseDate, moment(), undefined, '[]')} />
+              </Modal>
             </ModuleContainer>
           </Route>
           <Route path="*">
@@ -101,11 +143,55 @@ const TimeSliderContainer = styled.div`
   justify-content: center;
   height: 100%;
   width: 100%;
+  padding-bottom: 15px;
+`;
+
+const SliderWrapper = styled.div`
+  width: 80vw;
+`;
+
+const SliderLegendContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  
+  & > div:first-of-type {
+    transform: translateX(-40%);
+  }
+  
+  & > div:last-of-type {
+    transform: translateX(60%);
+  }
 `;
 
 const StyledSlider = styled(Slider)`
   pointer-events: auto;
-  width: 80vw;
-`
+  width: 100%;
+  & .ant-slider-track {
+    opacity: 0;
+  }
+  
+  .ant-slider-handle {
+    transform: translateX(-50%) scale(2) !important;
+    border-color: #177ddc;
+  }
+  
+  .ant-slider-rail {
+    background-color: #444;
+  }
+`;
+
+const CalendarButton = styled.div`
+  pointer-events: auto;
+  position: absolute;
+  right: 40px;
+  top: -35px;
+`;
+
+const CalendarButtonContainer = styled.div`
+  position: relative;
+  flex-grow: 1;
+  justify-content: flex-end;
+`;
 
 export default App;
